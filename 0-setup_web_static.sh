4752bfcd9805a247a26ff5df6f26179971e22d49
j-tyler <justin.marsh@holbertonschool.com>
2017-03-15 21:53:15 +0000

    Fix alias reversed directors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ mkdir -p /root/data/web_static/releases/test
 mkdir /root/data/web_static/shared
 echo 'Testing...' > /root/data/web_static/releases/test/index.html
 ln -sf /root/data/web_static/releases/test  /root/data/web_static/current
-chown -R ubuntu:ubuntu /data
+chown -R ubuntu:ubuntu /root/data
 echo 'user www-data;
 worker_processes 4;
 pid /run/nginx.pid;
@@ -36,8 +36,8 @@ http {
         location / {
             root /home;
         }
-        location /root/data/web_static/current/ {
-            alias /hbnb_static/;
+        location /hbnb_static/ {
+            alias /root/data/web_static/current/; 
 	   } 
         error_page 404 /404.html;
         location = /404.html {
@@ -48,4 +48,4 @@ http {
         }
     }
 }' | sudo tee /etc/nginx/nginx.conf > /dev/null
-sudo service restart nginx
+sudo service nginx restart
