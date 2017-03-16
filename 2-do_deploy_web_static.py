95b0e1c905781c3ca6da90229d2efa891adaff6d
j-tyler <justin.marsh@holbertonschool.com>
2017-03-16 06:29:31 +0000

    Few minor fileparsing changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,13 +29,14 @@ def do_deploy(archive_path):
         return False
     try:
         fn = archive_path.split('/')[-1]
-        version = fn.split[0]
+        version = fn.split('.')[0]
         put(archive_path, "/tmp/{:s}".format(fn))
-        sudo('tar xvzf {:s} -C /data/web_static/releases/{:s}'.format(
-             archive_path, version))
+        sudo("mkdir -p /data/web_static/releases/{:s}".format(version))
+        sudo('tar xvzf /tmp/{:s} -C /data/web_static/releases/{:s}'.format(
+             fn, version))
         sudo('rm -f /tmp/{:s}'.format(fn))
-        sudo('rm -f /data/web_server/curent')
-        sudo('ln -sf /data/web_static_releases/{:s} /data/web_server/current'.
+        sudo('rm -f /data/web_static/current')
+        sudo('ln -sf /data/web_static/releases/{:s} /data/web_static/current'.
              format(version))
     except:
         return False
